b825f74853416bb9f72078abea85c7975ac862f9
William Pittman <wspittman@gmail.com>
2025-02-17 20:53:27 -0800

    Initial SOAP calls

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello, world!");
+import * as soap from "soap";
+
+const wsdlUrl =
+  "https://wslwebservices.leg.wa.gov/LegislativeDocumentService.asmx?wsdl";
+
+async function getDocumentsByClass(
+  biennium: string,
+  documentClass: string,
+  namedLike: string
+) {
+  try {
+    // Create a SOAP client
+    const client = await soap.createClientAsync(wsdlUrl);
+
+    // Define the request parameters
+    const args = { biennium, documentClass, namedLike };
+
+    console.dir(args, { depth: null });
+    console.dir(client.describe(), { depth: null });
+
+    // Call the SOAP method
+    //const [result] = await client.GetDocumentClassesAsync(args);
+    //const [result] = await client.GetDocumentsAsync(args);
+    const [result] = await client.GetDocumentsByClassAsync(args);
+
+    console.dir(result, { depth: null });
+  } catch (error) {
+    console.error("Error calling SOAP API:", error);
+  }
+}
+
+// Example usage
+getDocumentsByClass("2025-26", "Bills", "1215");
